79a2ea4a7de6bb1211720036c8e3080c65bafa98
Oliver Kelso <oliver@pixelsoul.com>
2018-05-24 12:31:33 -0700

    Working on something new

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 var typed = new Typed("#typed", {
-	strings: ['', 'I am a web developer. ^2000', 'I am a geek. ^2000', 'I am a Portlander. ^2000', 'I am a gamer. ^2000', 'Hi! I\'m Oliver. ^25000'],
+	strings: ['', 'I am a web developer. ^2000', 'I am a geek. ^2000', 'I am a Portlander. ^2000', 'I am a gamer. ^2000', 'I <i class="fas fa-heart"></i> technology. ^2000', 'Hi! I\'m Oliver ^25000'],
 	smartBackspace: true,
 	typeSpeed: 50,
 	backSpeed: 40,
@@ -9,4 +9,5 @@ var typed = new Typed("#typed", {
 	showCursor: true,
   cursorChar: '|',
 	contentType: 'html',
+	startDelay: 0,
 });
