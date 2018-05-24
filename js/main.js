812ee384cb2bf7a2d0cf65a946f32266f924b456
Oliver Kelso <oliver@pixelsoul.com>
2018-05-24 11:18:01 -0700

    Work in progress

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var typed = new Typed("#typed", {
+	strings: ['', 'I am a web developer. ^2000', 'I am a geek. ^2000', 'I am a Portlander. ^2000', 'I am a gamer. ^2000', 'Hi! I\'m Oliver. ^25000'],
+	smartBackspace: true,
+	typeSpeed: 50,
+	backSpeed: 40,
+	shuffle: false,
+	loop: true,
+	loopCount: Infinity,
+	showCursor: true,
+  cursorChar: '|',
+	contentType: 'html',
+});
